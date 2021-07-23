18074a935ce94ba48228ea74044c6b5d564ebca2
Taner Topal <taner@adap.com>
2021-07-23 19:00:34 +0200

    wip

--- examples/quickstart_mlcube/run.sh
+++ examples/quickstart_mlcube/run.sh
@@ -2,14 +2,31 @@
 
 set -e
 
-pushd client_1/mlcube
+# Clone mlcube
+rm -rf mlcube && \
+git clone https://github.com/mlcommons/mlcube_examples.git && \
+cp -r mlcube_examples/mnist_openfl mlcube && \
+rm -rf mlcube_examples
+
+# Build mlcube
+pushd mlcube
 poetry run mlcube_docker configure --mlcube=. --platform=platforms/docker.yaml
 popd
 
+# Cleanup
+rm -rf clients/client_*
+
+# Start server
 python server.py &
 sleep 2 # Sleep for 2s to give the server enough time to start
-(cd client_1 && python client.py) &
-(cd client_2 && python client.py) &
+
+for i in `seq 0 3`; do
+    echo "Setting up client $i"
+    mkdir -p clients/client_$i
+    cp -r mlcube clients/client_$i/
+    cp -r client.py clients/client_$i/
+    python clients/client_$i/client.py &
+done
 
 # This will allow you to use CTRL+C to stop all background processes
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM
